04066fd1c1246364c510edeb9dd06919e51e7f4f
Jason Statham <karinov.nikolaj@inbox.ru>
2023-08-13 16:27:16 +0300

    responsEntity

--- src/main/java/com/example/laptopstore/controllers/Controller.java
+++ src/main/java/com/example/laptopstore/controllers/Controller.java
@@ -1,7 +1,9 @@
 package com.example.laptopstore.controllers;
 
 import com.example.laptopstore.DTO.LaptopDTO;
+import com.example.laptopstore.entity.Laptop;
 import com.example.laptopstore.services.LaptopService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.data.domain.*;
 import org.springframework.http.*;
@@ -14,13 +16,13 @@ public class Controller {
     public LaptopService laptopService;
 
     @GetMapping
-    public ResponseEntity getAllLaptops(
+    public ResponseEntity<List<Laptop>> getAllLaptops(
                                         @RequestParam(required = false, defaultValue = "0") int page,
                                         @RequestParam(required = false, defaultValue = "10") int size) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(laptopService.getAllLaptops(PageRequest.of(page, size)));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 
         }
     }
@@ -28,7 +30,7 @@ public class Controller {
 
 
     @PostMapping("/hibernate")
-    public ResponseEntity getLaptopPostFilters(@RequestBody LaptopDTO laptopDTO,
+    public ResponseEntity<List<Laptop>> getLaptopPostFilters(@RequestBody LaptopDTO laptopDTO,
                                                @RequestParam(required = false, defaultValue = "0") int page,
                                                @RequestParam(required = false, defaultValue = "10") int size) {
         return ResponseEntity.status(HttpStatus.OK)
